56809044583e4d9360e9e197d03e970b3edf48d9
SnehaMS17 <snehasiddegowda@gmail.com>
2026-01-21 16:05:38 +0530

    Added MongoDB connection and auth APIs

--- server/server.js
+++ server/server.js
@@ -3,11 +3,22 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// auth routes
+app.use("/api/auth", authRoutes);
 
 // test route
 app.get("/", (req, res) => {
